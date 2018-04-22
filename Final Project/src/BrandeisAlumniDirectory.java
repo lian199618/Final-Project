64b6b6e28b9b546d9b1984a7fe13aeb22c7f2166
Jaycee666 <36002032+Jaycee666@users.noreply.github.com>
2018-04-22 19:21:50 -0400

    image

--- Final Project/src/BrandeisAlumniDirectory.java
+++ Final Project/src/BrandeisAlumniDirectory.java
@@ -3,19 +3,22 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
+
 public class BrandeisAlumniDirectory{
-	public static void main(String[] args){
+	public static void main(String[] args) throws ClassNotFoundException {
 		SearchDirectory contactBook = new SearchDirectory();
 
 		JFrame window = new JFrame("Brandeis Alumni Directory");
 		JPanel content = new JPanel();
 		content.setLayout(new BorderLayout());
 
-		JLabel title = new JLabel("<html><h1>Brandeis Alumni Directory</h1></html>");
-		title.setForeground(new Color(0, 102, 255));
-		title.setBackground(Color.WHITE);
-		title.setHorizontalAlignment(SwingConstants.CENTER);
-		content.add(title, BorderLayout.PAGE_START);
+		ImageIcon title = new ImageIcon("../title.png");
+		JLabel logo = new JLabel(title, JLabel.CENTER);
+		// JLabel title = new JLabel("<html><h1>Brandeis Alumni Directory</h1></html>");
+		// title.setForeground(new Color(0, 102, 255));
+		// title.setBackground(Color.WHITE);
+		// title.setHorizontalAlignment(SwingConstants.CENTER);
+		 content.add(logo, BorderLayout.PAGE_START);
 
 		JPanel inputPage = new InputPage();
 		JPanel searchPage = new SearchPage();
@@ -28,6 +31,6 @@ public class BrandeisAlumniDirectory{
 		window.setSize(565,525);
 		window.setLocation(100,100);
 		window.setVisible(true);
-
+		window.setResizable(false);
 	}
 }
